e18aff0571fb281459b13ed5d55a5ce10664cb87
Jorge Rito <94419226+JorgeRito@users.noreply.github.com>
2025-10-10 07:08:26 -0600

    create section comp

--- frontend/ECC/src/components/TopBanner/TopBanner.tsx
+++ frontend/ECC/src/components/TopBanner/TopBanner.tsx
@@ -1,17 +1,20 @@
 import "./TopBanner.css";
 import NavBar from "../NavBar/CustomButton/NavBar";
+
 export default function TopBanner() {
   return (
     <>
       <div className="top-banner">
-        <img src="" />
+        <div style={{display: 'flex', justifyContent: "center"}}>
+        <img className="logo" src={"frontend/ECC/src/assets/ECC_Logo.jpg"} alt="Image not loading"/>
         <div>
           <label className="top-banner-text">
             <b>English Chat Class</b>
           </label>
           <div>
-            <label className="quote">Lema de la escuela</label>
+            <label className="quote">¡Habla Inglés sin salir de casa!</label>
           </div>
+        </div>
         </div>
       </div>
 
