e834343aacf2b21438cea4e25a71401be8791664
Jorge Rito <94419226+JorgeRito@users.noreply.github.com>
2025-10-21 20:12:55 -0600

    images path update

--- frontend/ECC/src/App.tsx
+++ frontend/ECC/src/App.tsx
@@ -25,7 +25,7 @@ function App() {
         "Bienvenido a English Chat Class, el espacio donde aprender inglés se convierte en una experiencia dinámica, práctica y enfocada en la conversación real. Nuestras clases en vivo te permitirán mejorar tu fluidez, comprensión y confianza al hablar, con la guía de profesores calificados y un ambiente interactivo que se adapta a tu ritmo y objetivos personales. Aprende inglés desde cualquier lugar y conéctate con personas que, como tú, buscan avanzar profesional y personalmente a través del idioma.",
       content2: [],
       className: "left",
-      img: "opportunities.jpg"
+      img: "/english-chat-class/opportunities.jpg"
     },
     {
       title: "¿Quienes somos?",
@@ -33,7 +33,7 @@ function App() {
         "Somos una escuela de inglés 100% en línea especializada en clases en vivo. Creemos que la mejor forma de dominar un idioma es a través de la práctica constante y la interacción real. Nuestro método se centra en la comunicación efectiva: no solo aprenderás gramática o vocabulario, sino que desarrollarás la habilidad de pensar y expresarte en inglés con naturalidad.",
       content2: [],
       className: "right",
-      img: "teacher.png"
+      img: "/english-chat-class/teacher.png"
     },
     {
       title: "Conoce al equipo",
@@ -41,7 +41,7 @@ function App() {
         "Nuestro equipo está formado por profesores certificados y con amplia experiencia en la enseñanza del inglés a estudiantes de distintos niveles y contextos. Cada miembro de English Chat Class comparte un mismo objetivo: ayudarte a alcanzar la confianza necesaria para comunicarte en inglés en cualquier situación. Más que maestros, somos mentores y compañeros de aprendizaje que te guiarán con paciencia, motivación y estrategias prácticas adaptadas a tus necesidades.",
       content2: [],
       className: "left",
-      img: "class.png"
+      img: "/english-chat-class/class.png"
     }
   ];
 
@@ -80,7 +80,7 @@ function App() {
             </p>
           </div>
           <div className="img-container">
-            <img src="contact.jpg"></img>
+            <img src="/english-chat-class/contact.jpg"></img>
           </div>
         </div>
       </div>
