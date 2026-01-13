f6545c9fd0ff30bb1b974b20a1857dca52c9330b
Jorge Rito <94419226+JorgeRito@users.noreply.github.com>
2026-01-13 16:21:46 -0600

    path fix
    common path for some files

--- frontend/ECC/src/components/CreateStudent/index.tsx
+++ frontend/ECC/src/components/CreateStudent/index.tsx
@@ -1,4 +1,4 @@
-import "C:\\Users\\061571\\OneDrive - Avnet\\Documents\\GitHub\\english-chat-class\\frontend\\ECC\\src\\components\\CreateStudent\\form.css"
+import "./form.css"
 import StudentsTable from "../StudentsTable/StudentsTable.tsx";
 import type { updateDataType } from "../StudentsTable/StudentsTable.tsx";
 import { useState } from "react";
