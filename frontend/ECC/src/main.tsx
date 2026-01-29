c2dd0bdf404bb27b7ed0304717fa9959484a59c4
Jorge Rito <94419226+JorgeRito@users.noreply.github.com>
2026-01-28 18:20:58 -0600

    dashboard creation

--- frontend/ECC/src/main.tsx
+++ frontend/ECC/src/main.tsx
@@ -6,7 +6,7 @@ import PublicSite from "./App.tsx";
 import { Dashboard } from "./components/Dashboard/Dashboard.tsx";
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <PublicSite />
-    {/* <Dashboard/> */}
+    {/* <PublicSite /> */}
+    <Dashboard/>
   </StrictMode>
 );
