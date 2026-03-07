b025898e304a5e2f9e344047c9a952efe52d2437
Jorge Rito <94419226+JorgeRito@users.noreply.github.com>
2026-03-07 08:41:56 -0600

    routes fix

--- src/main.tsx
+++ src/main.tsx
@@ -8,8 +8,8 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import {CreateUserForm} from "./components/CreateStudent/index.tsx";
 
 const router = createBrowserRouter([
-  {path : '/', element: <PublicSite/>},
-  {path : '/dashboard', element: <CreateUserForm/>}
+  {path : '/english-chat-class', element: <PublicSite/>},
+  {path : '/english-chat-class/dashboard', element: <CreateUserForm/>}
 ])
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
