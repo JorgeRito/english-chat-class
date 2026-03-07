fe44f5f98f2c6198038c7a7187c6780560bf58f7
Jorge Rito <94419226+JorgeRito@users.noreply.github.com>
2026-03-06 21:05:11 -0600

    app update

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,10 @@ import {Horarios} from "./components/Horarios/Horarios";
 import {Testimonios} from "./components/Testimonios/Testimonios";
 import {Contacto} from "./components/Contacto/Contacto";
 import {Header} from "./components/Header/Header";
-// import {WeeklySchedule} from "./components/WeeklySchedule/WeeklySchedule";
-// import {NewSchedule} from "./newSchedule/newSchedule";
-// import {CreateUserForm} from "./components/CreateStudent";
-// import {Scheduler} from "./components/Scheduler/Scheduler";
+import {WeeklySchedule} from "./components/WeeklySchedule/WeeklySchedule";
+import {NewSchedule} from "./newSchedule/newSchedule";
+import {CreateUserForm} from "./components/CreateStudent";
+import {Scheduler} from "./components/Scheduler/Scheduler";
 function PublicSite() {
   return (
     <div className="container">
