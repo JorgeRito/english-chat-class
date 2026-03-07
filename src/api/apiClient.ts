466a3cf5249dc6eabbcd45aff1767e866bdc5ccd
Jorge Rito <94419226+JorgeRito@users.noreply.github.com>
2026-03-07 08:12:46 -0600

    remote client

--- src/api/apiClient.ts
+++ src/api/apiClient.ts
@@ -4,3 +4,8 @@ export const apiClientLocal = axios.create({
     baseURL: "http://localhost:5000",
     timeout: 10000,
 });
+
+export const apiClientRemote = axios.create({
+    baseURL: "",
+    timeout: 10000,
+})
